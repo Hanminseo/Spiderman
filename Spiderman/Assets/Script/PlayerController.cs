3ba932f94380d2c2066b637038d94a573f38f510
Hanminseo <hm5260@naver.com>
2019-05-27 14:48:45 +0900

    두번째 - 에셋파일을 업로드 했다, 스파이더맨 이동기능 완료

---
+++ Spiderman/Assets/Script/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 15f;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    public void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    public void FixedUpdate()
+    {
+        var h = Input.GetAxisRaw("Horizontal");
+        var v = Input.GetAxisRaw("Vertical");
+        var direction = new Vector3(h, 0.0f, v);
+        direction.Normalize();
+        var deltaPos = direction * speed * Time.deltaTime;
+        var nextPos = transform.position + deltaPos;
+        rb.MovePosition(nextPos);
+    }
+}
